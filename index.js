9076188dd07fc9af0262b1fa0efcbf13356fc233
Jak Major <hellojakkaiser@gmail.com>
2021-08-30 10:38:51 -0700

    commit tree 2de62ff8479dae37856b7df78b79460aaf708993

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let mod = num3 % num4;
+let max = Math.max(1,5,10,20);
